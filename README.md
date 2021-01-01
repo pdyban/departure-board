ecbf4dd42341fefabc47e638ffa7d782b3403ae1
Pavlo Dyban <pdyban@gmail.com>
2021-01-01 23:27:46 +0100

    Initial commit with Vue.js

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Departure board
 A minimalistic web-based departure board with an overview of upcoming scheduled departures from a selected station in Greater Berlin Area. This is a test project to acquaint myself with Vue.js and state-of-the-art Javascript best practices.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <b-navbar toggleable="lg" type="light" id="navbar">
+        <b-container>
+            <b-navbar-brand href="#"><img src="../assets/images/train.svg" width="30" height="30" alt=""> Departure Board</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+              <!-- Right aligned nav items -->
+              <b-navbar-nav>
+                <b-nav-item href="#">Settings</b-nav-item>
+              </b-navbar-nav>
+            </b-collapse>
+          </b-container>
+      </b-navbar>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+  methods: {
+  }
+}
+</script>
+<style>
+#navbar {
+  background-color: #ffcc33;
+}
+</style>
